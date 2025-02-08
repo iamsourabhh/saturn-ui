923ebcede3a2fd2b771461bc5504de96b3cb5418
sourabhparmar <sourabh.parmar@acceldata.io>
2025-02-08 17:15:04 +0530

    refactor: restructure project by removing App component and styles, adding sample Button component and build as a library

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import './App.scss';
-
-function App() {
-  return <h1>Saturn UI</h1>;
-}
-
-export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ outDir: 'dist/types', insertTypesEntry: true })],
+
+  build: {
+    lib: {
+      entry: 'src/index.tsx',
+      name: 'SaturnUi',
+      fileName: (format) => `saturn-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
